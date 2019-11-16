9634a1a9045438e21784775d27b2dfe9083a1725
Homère <homerebourgeois@hotmail.fr>
2019-11-16 20:56:25 +0100

    boucle d'attaque de base du minotaure crée

--- ex_wife/Assets/Scripts/CharController.cs
+++ ex_wife/Assets/Scripts/CharController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharController : MonoBehaviour
 {
     public float speed = 2.0f;
     public Animator animator;
---
+++ ex_wife/Assets/Scripts/Minotaur.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minotaur : Monster
+{
+    public State state;
+    public GameObject target;
+    public float chargingDistance;
+    public float attackingDistance;
+    public Animator anim;
+
+
+    private void Start()
+    {
+        state = State.chasing;
+    }
+
+    private void Update()
+    {
+        capacityCooldown -= Time.deltaTime;
+        Vector3 direction = target.transform.position - transform.position;
+        switch(state)
+        {       
+            case State.chasing:
+                transform.position += direction.normalized * Speed * Time.deltaTime;
+                if(CapacityCooldown < 0 && direction.sqrMagnitude < chargingDistance * chargingDistance)
+                {
+                    anim.SetTrigger("Charge");
+                    capacityCooldown = maxCapacityCooldown;
+                    state = State.charging;
+                }
+                if (direction.sqrMagnitude < attackingDistance * attackingDistance)
+                {
+                    anim.SetTrigger("Attack");
+                    state = State.attacking;
+                }
+                break;
+            case State.attacking:
+                direction = target.transform.position - transform.position;
+                if (direction.sqrMagnitude > attackingDistance * attackingDistance)
+                {
+                    state = State.chasing;
+                }
+                break;
+            case State.charging:
+                direction = target.transform.position - transform.position;
+                transform.position += direction.normalized * Speed * 3 * Time.deltaTime;
+                break;
+        }
+    }
+
+    public enum State
+    {
+        idle,
+        chasing,
+        attacking,
+        charging,
+        hit,
+        death,
+    }
+}
