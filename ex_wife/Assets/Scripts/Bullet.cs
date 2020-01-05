af17b415b3caa53658f3f13661d352fd9e8d67e7
Homère <homerebourgeois@hotmail.fr>
2020-01-05 14:48:26 +0100

    balles ok normalement

--- ex_wife/Assets/Scripts/Bullet.cs
+++ ex_wife/Assets/Scripts/Bullet.cs
@@ -6,6 +6,10 @@ public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
+    public float damage;
+    public float explosionRadius;
+    public Animator anim;
+    bool targetHit = false;
     void Start()
     {
         
@@ -14,6 +18,29 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.position += this.transform.up * speed * Time.deltaTime;
+        if(!targetHit)
+        {
+            this.transform.position += this.transform.right * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Monster" || collision.gameObject.tag == "Boss")
+        {
+            targetHit = true;
+            anim.enabled = true;
+            GetComponent<Collider2D>().enabled = false;
+            Collider2D[] hitMonsters = Physics2D.OverlapCircleAll(transform.position,explosionRadius);
+            foreach(Collider2D col in hitMonsters)
+            {
+                col.GetComponent<Unit>().TakeDamage(damage);
+            }
+        }
+    }
+
+    private void DestroyObject()
+    {
+        Destroy(gameObject);
     }
 }
